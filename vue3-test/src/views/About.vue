a7375656b8c857e5e0577942bf1c48d71e925bfd
HanLiang <ihan_liang@163.com>
2020-06-19 18:23:36 +0800

    vue3

--- vue3-test/src/views/About.vue
+++ vue3-test/src/views/About.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <button @click="increment">
+    Count is: {{ state.count }}, double is: {{ state.double }}
+  </button>
+  <div>{{a}}</div>
+  <div>{{b}}</div>
 </template>
+
+<script>
+  import { reactive, computed,ref, onMounted, onUnmounted  } from 'vue'
+
+  function useMousePosition() {
+    const x = ref(0)
+    const y = ref(0)
+
+    function update(e) {
+      x.value = e.pageX
+      y.value = e.pageY
+    }
+
+    onMounted(() => {
+      window.addEventListener('mousemove', update)
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('mousemove', update)
+    })
+
+    return { x, y }
+  }
+
+  export default {
+    setup() {
+      const state = reactive({
+        count: 0,
+        double: computed(() => state.count * 2),
+      })
+
+      function increment() {
+        state.count++
+      }
+
+      const { x:a, y:b } = useMousePosition()
+      console.log(a)
+
+      return {
+        a, b,
+        state,
+        increment,
+      }
+    }
+  }
+</script>
