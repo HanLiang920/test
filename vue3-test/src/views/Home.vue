aaee3a282fc9171eceb316b948c6610a4a86a765
HanLiang <ihan_liang@163.com>
2020-07-10 10:12:25 +0800

    导出excel

--- vue3-test/src/views/Home.vue
+++ vue3-test/src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" />
+    <input type="text" />
+    <button @click="dowload">下载</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Excel from 'exceljs/dist/exceljs.min.js'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    dowload() {
+      const workbook = new Excel.Workbook()
+      workbook.created = new Date()
+      workbook.modified = new Date()
+      const sheet = workbook.addWorksheet('测试导出表')
+      sheet.properties.defaultRowHeight = 25
+
+      sheet.columns = [
+        { header: '编号', key: 'id', width: 25 },
+        { header: '姓名', key: 'name', width: 30 },
+        { header: '年龄', key: 'age', width: 30 },
+        { header: '性别', key: 'gender', width: 30 },
+      ]
+      sheet.addRow({ id: 1, name: '小明', age: 26, gender: '男' })
+      sheet.addRow({ id: 2, name: '小红', age: 27, gender: '女' })
+      sheet.addRow({ id: 3, name: '小话', age: 25, gender: '男' })
+
+      workbook.xlsx
+        .writeBuffer()
+        .then((buffer) => {
+          // buffer --> blob
+          const blob = new Blob([buffer], { type: 'application/vnd.ms-excel' })
+          let link = document.createElement('a')
+          link.download = 'download.xlsx'
+          link.target = 'blank'
+          // blob --> url
+          link.href = URL.createObjectURL(blob)
+          link.click()
+        })
+        .catch((err) => {
+          throw err
+        })
+    },
+  },
 }
 </script>
