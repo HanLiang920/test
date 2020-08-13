998579cb1d8fba795322e14c9da112502bcb1dfc
HanLiang <ihan_liang@163.com>
2020-08-13 17:55:56 +0800

    swagger

--- nest-test/src/cats/cats.controller.ts
+++ nest-test/src/cats/cats.controller.ts
@@ -1,21 +1,34 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
-import { CreateCatDto } from './dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CatsService } from './cats.service';
+import { Cat } from './classes/cat.class';
+import { CreateCatDto } from './dto/create-cat.dto';
 
+@ApiBearerAuth()
+@ApiTags('cats')
 @Controller('cats')
 export class CatsController {
-  @Get()
-  findAll(): string {
-    return 'This action returns all cats2';
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create cat' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+    return this.catsService.create(createCatDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return `This action returns a #${id} cat`;
-  }
-
-  @Post()
-  async create(@Body() createCatDto: CreateCatDto): Promise<string> {
-    console.log(createCatDto);
-    return 'This action adds a new cat';
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: Cat,
+  })
+  findOne(@Param('id') id: string): Cat {
+    return this.catsService.findOne(+id);
   }
 }
